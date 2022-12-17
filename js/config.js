df9823ab7869c5c3dc90351fc125c7e56010387c
JesuFigueredo <116711727+JesuFigueredo@users.noreply.github.com>
2022-12-17 23:07:24 +0100

    config

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-//const enlace = "https://strapi-1uau.onrender.com/api";
-const enlace = "http://localhost:1337/api";
+const enlace = "https://strapi-1uau.onrender.com/api";
+//const enlace = "http://localhost:1337/api";
 
 
 
